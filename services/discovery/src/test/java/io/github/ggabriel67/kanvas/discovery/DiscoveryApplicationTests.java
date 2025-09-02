d72aa12f548549377b3cd4b1ece36fdbfa7d4bba
Gabriel Gomulka <gabrielgomulka123@gmail.com>
2025-09-02 23:47:36 +0200

    Create and configure discovery service

---
+++ services/discovery/src/test/java/io/github/ggabriel67/kanvas/discovery/DiscoveryApplicationTests.java
@@ -0,0 +1,13 @@
+package io.github.ggabriel67.kanvas.discovery;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DiscoveryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
