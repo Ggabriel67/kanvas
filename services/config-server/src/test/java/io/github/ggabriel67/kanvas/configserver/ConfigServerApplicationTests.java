ed1ad5c1c2607b609a9401add0c90f22a39af375
Gabriel Gomulka <gabrielgomulka123@gmail.com>
2025-09-02 23:16:48 +0200

    Create config server service

---
+++ services/config-server/src/test/java/io/github/ggabriel67/kanvas/configserver/ConfigServerApplicationTests.java
@@ -0,0 +1,13 @@
+package io.github.ggabriel67.kanvas.configserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConfigServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
