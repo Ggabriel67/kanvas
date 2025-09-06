8c2b7f7486eeceda8b028a59fdf0195235abc74f
Gabriel Gomulka <gabrielgomulka123@gmail.com>
2025-09-06 19:06:50 +0200

    Create and configure api gateway

---
+++ services/gateway/src/test/java/io/github/ggabriel67/kanvas/gateway/GatewayApplicationTests.java
@@ -0,0 +1,13 @@
+package io.github.ggabriel67.kanvas.gateway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GatewayApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
