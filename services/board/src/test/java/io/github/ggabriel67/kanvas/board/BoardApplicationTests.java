bce3f743d05712586621ed8becc2a208642241c6
Gabriel Gomulka <gabrielgomulka123@gmail.com>
2025-09-07 22:31:05 +0200

    Create and configure board service

---
+++ services/board/src/test/java/io/github/ggabriel67/kanvas/board/BoardApplicationTests.java
@@ -0,0 +1,13 @@
+package io.github.ggabriel67.kanvas.board;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BoardApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
