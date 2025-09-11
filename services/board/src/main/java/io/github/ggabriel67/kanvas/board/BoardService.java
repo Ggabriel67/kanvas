ce746a8138829215afc73c91a268c42718c5cf68
Gabriel Gomulka <gabrielgomulka123@gmail.com>
2025-09-11 18:08:21 +0200

    Update createBoard method by adding board creator as board ADMIN

--- services/board/src/main/java/io/github/ggabriel67/kanvas/board/BoardService.java
+++ services/board/src/main/java/io/github/ggabriel67/kanvas/board/BoardService.java
@@ -1,5 +1,8 @@
 package io.github.ggabriel67.kanvas.board;
 
+import io.github.ggabriel67.kanvas.authorization.board.BoardRole;
+import io.github.ggabriel67.kanvas.board.member.BoardMember;
+import io.github.ggabriel67.kanvas.board.member.BoardMemberRepository;
 import io.github.ggabriel67.kanvas.exception.NameAlreadyInUseException;
 import io.github.ggabriel67.kanvas.exception.WorkspaceNotFoundException;
 import io.github.ggabriel67.kanvas.user.User;
@@ -16,6 +19,7 @@ public class BoardService
     private final BoardRepository boardRepository;
     private final UserService userService;
     private final WorkspaceRepository workspaceRepository;
+    private final BoardMemberRepository memberRepository;
 
     public void createBoard(BoardRequest request) {
         User user = userService.getUserById(request.createdById());
@@ -27,13 +31,20 @@ public class BoardService
             throw new NameAlreadyInUseException("Board with name " +  request.name() + " already exists in this workspace");
         }
 
-        boardRepository.save(
-                Board.builder()
+        Board board = boardRepository.save(Board.builder()
                         .createdBy(user)
                         .workspace(workspace)
                         .name(request.name())
                         .description(request.description())
                         .visibility(request.visibility())
+                        .build()
+        );
+
+        memberRepository.save(
+                BoardMember.builder()
+                        .user(user)
+                        .board(board)
+                        .role(BoardRole.ADMIN)
                         .build()
         );
     }
--- services/board/src/main/java/io/github/ggabriel67/kanvas/board/member/BoardMemberRepository.java
+++ services/board/src/main/java/io/github/ggabriel67/kanvas/board/member/BoardMemberRepository.java
@@ -1,9 +1,10 @@
 package io.github.ggabriel67.kanvas.board.member;
 
+import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public class BoardMemberRepository
+public interface BoardMemberRepository extends JpaRepository<BoardMember, Integer>
 {
 
 }
