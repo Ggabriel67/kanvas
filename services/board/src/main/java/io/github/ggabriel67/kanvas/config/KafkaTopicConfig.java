6c4ab86793388a276a002b5dd398fd582a86c942
Gabriel Gomulka <gabrielgomulka123@gmail.com>
2025-09-13 17:38:23 +0200

    Create kafka topics in board service

---
+++ services/board/src/main/java/io/github/ggabriel67/kanvas/config/KafkaTopicConfig.java
@@ -0,0 +1,25 @@
+package io.github.ggabriel67.kanvas.config;
+
+import org.apache.kafka.clients.admin.NewTopic;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.kafka.config.TopicBuilder;
+
+@Configuration
+public class KafkaTopicConfig
+{
+    @Bean
+    public NewTopic invitationEventsTopic() {
+        return TopicBuilder.name("invitation.events").build();
+    }
+
+    @Bean
+    public NewTopic boardEventsTopic() {
+        return TopicBuilder.name("board.events").build();
+    }
+
+    @Bean
+    public NewTopic boardPresenceEventsTopic() {
+        return TopicBuilder.name("board.presence.events").build();
+    }
+}
