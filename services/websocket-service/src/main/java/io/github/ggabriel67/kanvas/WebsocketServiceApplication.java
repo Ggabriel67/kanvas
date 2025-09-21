3ec1f9b3d2de3bfc9861d3c279e35221dbe986b9
Gabriel Gomulka <gabrielgomulka123@gmail.com>
2025-09-21 15:27:02 +0200

    Create and configure the websocket service

---
+++ services/websocket-service/src/main/java/io/github/ggabriel67/kanvas/WebsocketServiceApplication.java
@@ -0,0 +1,13 @@
+package io.github.ggabriel67.kanvas;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebsocketServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WebsocketServiceApplication.class, args);
+	}
+
+}
