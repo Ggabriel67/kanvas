f0ad207aaa62c2ee5329da57f06aba49421d0c3d
Gabriel Gomulka <gabrielgomulka123@gmail.com>
2025-09-29 14:41:01 +0200

    Implement sign in page

--- frontend/src/api/auth.ts
+++ frontend/src/api/auth.ts
@@ -12,14 +12,31 @@ export type RegistrationRequest = {
   password: string;
 };
 
+export type AuthenticationRequest = {
+  email: string;
+  password: string;
+}
+
 export async function registerUser(data: RegistrationRequest) {
   try {
 		const res = await api.post("/register", data);
 		return res.data;
   } catch (error: any) {
     if (error.response) {
-    throw new Error(error.response.data.error || "Registration failed");
+      throw new Error(error.response.data.error || "Registration failed");
     }
     throw error;
-  }	
+  }
+}
+
+export async function authenticateUser(data: AuthenticationRequest) {
+  try {
+    const response = await api.post<{accessToken: string;}>("/authenticate", data);
+    return response.data.accessToken;
+  } catch (error: any) {
+    if (error.response) {
+      throw new Error(error.response.data.error || "Authentication failed");
+    }
+    throw error;
+  }
 }
